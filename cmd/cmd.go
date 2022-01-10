b1cbdae26f833486ebd48d56bc56e109198631b4
Mak Muftic <mak@chainsafe.io>
2022-01-10 18:38:56 +0100

    Initial work

---
+++ cmd/cmd.go
@@ -0,0 +1,41 @@
+// Copyright 2021 ChainSafe Systems
+// SPDX-License-Identifier: LGPL-3.0-only
+
+package cmd
+
+import (
+	cCLI "github.com/ChainSafe/chainbridge-celo-module/cli"
+	"github.com/ChainSafe/chainbridge-celo-module/example"
+	evmCLI "github.com/ChainSafe/chainbridge-core/chains/evm/cli"
+	"github.com/ChainSafe/chainbridge-core/flags"
+	"github.com/rs/zerolog/log"
+	"github.com/spf13/cobra"
+)
+
+var (
+	rootCMD = &cobra.Command{
+		Use: "",
+	}
+	runCMD = &cobra.Command{
+		Use:   "run",
+		Short: "Run example app",
+		Long:  "Run example app",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			if err := example.Run(); err != nil {
+				return err
+			}
+			return nil
+		},
+	}
+)
+
+func init() {
+	flags.BindFlags(rootCMD)
+}
+
+func Execute() {
+	rootCMD.AddCommand(runCMD, cCLI.CeloRootCLI, evmCLI.EvmRootCLI)
+	if err := rootCMD.Execute(); err != nil {
+		log.Fatal().Err(err).Msg("failed to execute root cmd")
+	}
+}
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/ChainSafe/chainbridge-celo-module/cmd"
+
+func main() {
+	cmd.Execute()
+}
