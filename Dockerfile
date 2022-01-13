197f48c3c9d8770b4918a259cdf3d956e27b52f3
waymobetta <jbroethke@gmail.com>
2022-01-12 18:42:19 -0800

    remove subkey dep, update docker-compose paths, add local CLI command

--- Dockerfile
+++ Dockerfile
@@ -6,15 +6,11 @@ ADD . /src
 WORKDIR /src
 RUN cd /src && echo $(ls -1 /src)
 RUN go mod download
-RUN go build -o /bridge .
+RUN go build -o /bridge e2e/celo-celo/example/main.go
 
 # # final stage
 FROM debian:stretch-slim
 RUN apt-get -y update && apt-get -y upgrade && apt-get install ca-certificates wget -y
-RUN wget -P /usr/local/bin/ https://chainbridge.ams3.digitaloceanspaces.com/subkey-rc6 \
-  && mv /usr/local/bin/subkey-rc6 /usr/local/bin/subkey \
-  && chmod +x /usr/local/bin/subkey
-RUN subkey --version
 
 COPY --from=builder /bridge ./
 RUN chmod +x ./bridge
---
+++ cli/local/local.go
@@ -0,0 +1,103 @@
+package local
+
+import (
+	"fmt"
+	"math/big"
+
+	"github.com/ChainSafe/chainbridge-celo-module/transaction"
+	"github.com/ChainSafe/chainbridge-core/chains/evm/calls/evmclient"
+	"github.com/ChainSafe/chainbridge-core/chains/evm/cli/local"
+
+	"github.com/spf13/cobra"
+)
+
+var LocalSetupCmd = &cobra.Command{
+	Use:   "local-setup",
+	Short: "Deploy and prefund a local bridge for testing",
+	Long:  "The local-setup command deploys a bridge, ERC20, ERC721 and generic handler contracts with preconfigured accounts and appropriate handlers",
+	RunE:  LocalSetupCLI,
+}
+
+// configuration
+var (
+	ethEndpoint1 = "http://localhost:8545"
+	ethEndpoint2 = "http://localhost:8547"
+	fabric1      = transaction.NewCeloTransaction
+	fabric2      = transaction.NewCeloTransaction
+)
+
+func LocalSetupCLI(cmd *cobra.Command, args []string) error {
+	// init client1
+	ethClient, err := evmclient.NewEVMClientFromParams(ethEndpoint1, local.EveKp.PrivateKey())
+	if err != nil {
+		return err
+	}
+
+	// init client2
+	ethClient2, err := evmclient.NewEVMClientFromParams(ethEndpoint2, local.EveKp.PrivateKey())
+	if err != nil {
+		return err
+	}
+
+	// chain 1
+	// domainsId: 0
+	config, err := local.PrepareLocalEVME2EEnv(ethClient, fabric1, 1, big.NewInt(1), local.EveKp.CommonAddress())
+	if err != nil {
+		return err
+	}
+
+	// chain 2
+	// domainId: 1
+	config2, err := local.PrepareLocalEVME2EEnv(ethClient2, fabric2, 2, big.NewInt(1), local.EveKp.CommonAddress())
+	if err != nil {
+		return err
+	}
+
+	prettyPrint(config, config2)
+
+	return nil
+}
+
+func prettyPrint(config, config2 local.EVME2EConfig) {
+	fmt.Printf(`
+===============================================
+🎉🎉🎉 ChainBridge Successfully Deployed 🎉🎉🎉
+
+- Chain 1 - 
+Bridge: %s
+ERC20: %s
+ERC20 Handler: %s
+ERC721: %s
+ERC721 Handler: %s
+Generic Handler: %s
+Asset Store: %s
+
+- Chain 2 -
+Bridge: %s
+ERC20: %s
+ERC20 Handler: %s
+ERC721: %s
+ERC721 Handler: %s
+Generic Handler: %s
+Asset Store: %s
+
+===============================================
+`,
+		// config
+		config.BridgeAddr,
+		config.Erc20Addr,
+		config.Erc20HandlerAddr,
+		config.Erc721Addr,
+		config.Erc721HandlerAddr,
+		config.GenericHandlerAddr,
+		config.AssetStoreAddr,
+		// config2
+		config2.BridgeAddr,
+		config2.Erc20Addr,
+		config2.Erc20HandlerAddr,
+		config.Erc721Addr,
+		config.Erc721HandlerAddr,
+		config2.GenericHandlerAddr,
+		config2.AssetStoreAddr,
+	)
+}
