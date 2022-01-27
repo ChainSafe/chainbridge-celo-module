68632ccf1b7fdb9e8f56e267612486964c3fb062
Matija Petrunic <matija.petrunic@fer.hr>
2022-01-27 16:49:55 +0100

    Use celo app instead of chainbridge-core app in tests

--- e2e/celo-celo/example/cmd/cmd.go
+++ e2e/celo-celo/example/cmd/cmd.go
@@ -6,8 +6,8 @@ package cmd
 import (
 	cCLI "github.com/ChainSafe/chainbridge-celo-module/cli"
 	"github.com/ChainSafe/chainbridge-celo-module/cli/local"
+	"github.com/ChainSafe/chainbridge-celo-module/e2e/celo-celo/example/app"
 	evmCLI "github.com/ChainSafe/chainbridge-core/chains/evm/cli"
-	"github.com/ChainSafe/chainbridge-core/e2e/evm-evm/example/app"
 	"github.com/ChainSafe/chainbridge-core/flags"
 	"github.com/rs/zerolog/log"
 	"github.com/spf13/cobra"
