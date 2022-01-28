2ff70f6e382822eb85373b6bc95295b01bc1473a
Matija Petrunic <matija.petrunic@fer.hr>
2022-01-28 14:04:36 +0100

    Set gas pricer as static

---
+++ chain.go
@@ -0,0 +1,52 @@
+package celo
+
+import (
+	"github.com/ChainSafe/chainbridge-core/chains/evm"
+	"github.com/ChainSafe/chainbridge-core/chains/evm/calls"
+	"github.com/ChainSafe/chainbridge-core/chains/evm/calls/contracts/bridge"
+	"github.com/ChainSafe/chainbridge-core/chains/evm/calls/evmclient"
+	"github.com/ChainSafe/chainbridge-core/chains/evm/calls/evmgaspricer"
+	"github.com/ChainSafe/chainbridge-core/chains/evm/calls/transactor/signAndSend"
+	"github.com/ChainSafe/chainbridge-core/chains/evm/listener"
+	"github.com/ChainSafe/chainbridge-core/chains/evm/voter"
+	"github.com/ChainSafe/chainbridge-core/config/chain"
+	"github.com/ChainSafe/chainbridge-core/store"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/rs/zerolog/log"
+)
+
+func SetupDefaultCeloChain(rawConfig map[string]interface{}, txFabric calls.TxFabric, blockstore *store.BlockStore) (*evm.EVMChain, error) {
+	config, err := chain.NewEVMConfig(rawConfig)
+	if err != nil {
+		return nil, err
+	}
+
+	client, err := evmclient.NewEVMClient(config)
+	if err != nil {
+		return nil, err
+	}
+
+	gasPricer := evmgaspricer.NewStaticGasPriceDeterminant(client, nil)
+	t := signAndSend.NewSignAndSendTransactor(txFabric, gasPricer, client)
+	bridgeContract := bridge.NewBridgeContract(client, common.HexToAddress(config.Bridge), t)
+
+	eventHandler := listener.NewETHEventHandler(*bridgeContract)
+	eventHandler.RegisterEventHandler(config.Erc20Handler, listener.Erc20EventHandler)
+	eventHandler.RegisterEventHandler(config.Erc721Handler, listener.Erc721EventHandler)
+	eventHandler.RegisterEventHandler(config.GenericHandler, listener.GenericEventHandler)
+	evmListener := listener.NewEVMListener(client, eventHandler, common.HexToAddress(config.Bridge))
+
+	mh := voter.NewEVMMessageHandler(*bridgeContract)
+	mh.RegisterMessageHandler(config.Erc20Handler, voter.ERC20MessageHandler)
+	mh.RegisterMessageHandler(config.Erc721Handler, voter.ERC721MessageHandler)
+	mh.RegisterMessageHandler(config.GenericHandler, voter.GenericMessageHandler)
+
+	var evmVoter *voter.EVMVoter
+	evmVoter, err = voter.NewVoterWithSubscription(mh, client, bridgeContract)
+	if err != nil {
+		log.Error().Msgf("failed creating voter with subscription: %s. Falling back to default voter.", err.Error())
+		evmVoter = voter.NewVoter(mh, client, bridgeContract)
+	}
+
+	return evm.NewEVMChain(evmListener, evmVoter, blockstore, config), nil
+}
